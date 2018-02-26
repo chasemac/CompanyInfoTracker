b2576645022f474f94f810c027a933d2cfb3129c
Chase McElroy <ichasem@gmail.com>
2018-02-26 14:05:52 -0800

    Added date picker and formatted dates

--- CompanyInfoTracker/CompaniesController.swift
+++ CompanyInfoTracker/CompaniesController.swift
@@ -141,7 +141,19 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelagat
         
         let company = companies[indexPath.row]
         
-        cell.textLabel?.text = company.name
+        if let name = company.name, let founded = company.founded {
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMM dd, YYYY"
+            
+            let foundedDateString = dateFormatter.string(from: founded)
+  //          let locale = Locale(identifier: "EN")
+            
+            let dateString =  "\(name) - Founded: \(foundedDateString)"
+            cell.textLabel?.text = dateString
+        } else {
+            cell.textLabel?.text = company.name
+        }
         cell.textLabel?.textColor = .white
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         return cell
--- CompanyInfoTracker/CreateCompanyController.swift
+++ CompanyInfoTracker/CreateCompanyController.swift
@@ -20,6 +20,8 @@ class CreateCompanyController: UIViewController {
     var company: Company? {
         didSet {
             nameTextField.text = company?.name
+            guard let founded = company?.founded else {return}
+            datePicker.date = founded
         }
     }
     
@@ -36,10 +38,17 @@ class CreateCompanyController: UIViewController {
     let nameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Placeholder"
-        textField
-            .translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
 
         return textField
+    }()
+    
+    let datePicker: UIDatePicker = {
+        let dp = UIDatePicker()
+        dp.datePickerMode = .date
+        dp.translatesAutoresizingMaskIntoConstraints = false
+        return dp
+
     }()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -73,6 +82,7 @@ class CreateCompanyController: UIViewController {
     private func saveCompanyChanges() {
         let context = CoreDataManager.shared.persistentContainer.viewContext
         company?.name = nameTextField.text
+        company?.founded = datePicker.date
         do {
             try context.save()
             //success
@@ -89,6 +99,7 @@ class CreateCompanyController: UIViewController {
         let company = NSEntityDescription.insertNewObject(forEntityName: "Company", into: context)
         guard let name = self.nameTextField.text else {return}
         company.setValue(name, forKey: "name")
+        company.setValue(datePicker.date, forKey: "founded")
         
         //perform save
         do {
@@ -112,7 +123,7 @@ class CreateCompanyController: UIViewController {
         lightBlueBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         lightBlueBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         lightBlueBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         
         view.addSubview(nameLabel)
         nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -125,6 +136,12 @@ class CreateCompanyController: UIViewController {
         nameTextField.leftAnchor.constraint(equalTo: nameLabel.rightAnchor).isActive = true
         nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        
+        view.addSubview(datePicker)
+        datePicker.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        datePicker.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        datePicker.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        datePicker.bottomAnchor.constraint(equalTo: lightBlueBackgroundView.bottomAnchor).isActive = true
     }
     
     @objc func handleCancel() {
